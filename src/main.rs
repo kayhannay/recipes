2aae00521ba641167a98e3aa85c44cfece53ae6b
Kay Hannay <klinux@hannay.de>
2018-08-27 21:28:04 +0200

    Some minor refactoring and cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@
 
 extern crate rocket;
 extern crate rocket_contrib;
-#[macro_use] extern crate mysql;
+extern crate mysql;
 #[macro_use] extern crate serde_derive;
 extern crate chrono;
 
 use rocket_contrib::Template;
-use rocket::response::NamedFile;
-use std::io::Error;
-use std::collections::HashMap;
 use mysql as my;
-//use mysql::chrono::DateTime;
-//use mysql::chrono::Utc;
 use chrono::NaiveDateTime;
 
 #[derive(Debug, PartialEq, Eq, Serialize)]
@@ -38,12 +33,6 @@ struct RecipeName {
 }
 
 #[derive(Serialize)]
-struct TemplateContext {
-    name: String,
-    items: Vec<String>,
-}
-
-#[derive(Serialize)]
 struct RecipeOverviewModel {
     recipe_names: Vec<RecipeName>,
 }
@@ -64,7 +53,7 @@ fn index() -> Template {
 #[get("/recipe/<id>")]
 fn hello(id: i32) -> Template {
     let pool = init_db();
-    let _recipe = get_recipe(pool, id);
+    let _recipe = get_recipe(pool, &id);
     let model = RecipeModel {
         recipe: _recipe.unwrap(),
     };
@@ -96,23 +85,19 @@ fn get_recipes_names(pool: mysql::Pool) -> Vec<RecipeName> {
     return recipes;
 }
 
-fn get_recipe(pool: mysql::Pool, id: i32) -> Option<Recipe> {
+fn get_recipe(pool: mysql::Pool, id: &i32) -> Option<Recipe> {
     let mut stmt = pool.prepare("SELECT * FROM rezepte WHERE id=?").unwrap();
     let mut recipe = None;
-    for row in stmt.execute((id,)).unwrap() {
+    for row in stmt.execute((*id,)).unwrap() {
         let mut row = row.unwrap();
-        let ingredients: String = row.take("ingredients").unwrap();
-        let preparation: String = row.take("preparation").unwrap();
         recipe = Some(Recipe {
             id: row.take("id").unwrap(),
             category: row.take("category").unwrap(),
             name: row.take("name").unwrap(),
-            ingredients: ingredients.replace("\n", "<br />"),
-            preparation: preparation.replace("\n", "<br />"),
-            //preparation: row.take("preparation").unwrap(),
+            ingredients: replace_breaklines(row.take("ingredients").unwrap()),
+            preparation: replace_breaklines(row.take("preparation").unwrap()),
             experience: row.take("experience").unwrap(),
             time_needed: row.take("time_needed").unwrap_or("".to_string()),
-            //time_needed: "".to_string(),
             number_people: row.take("number_people").unwrap(),
             created: row.take("created").unwrap(),
             owner: row.take("owner").unwrap(),
@@ -122,32 +107,8 @@ fn get_recipe(pool: mysql::Pool, id: i32) -> Option<Recipe> {
     return recipe;
 }
 
-fn get_recipes(pool: mysql::Pool) -> Vec<Recipe> {
-    let recipes: Vec<Recipe> =
-        pool.prep_exec("SELECT * from rezepte", ())
-            .map(|result| { // In this closure we will map `QueryResult` to `Vec<Payment>`
-                // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
-                // will map each `MyResult` to contained `row` (no proper error handling)
-                // and second call to `map` will map each `row` to `Payment`
-                result.map(|x| x.unwrap()).map(|row| {
-                    // ⚠️ Note that from_row will panic if you don't follow your schema
-                    let (id, name, ingredients, preparation, experience, time_needed, number_people, created, owner, rights, category) = my::from_row(row);
-                    Recipe {
-                        id: id,
-                        category: category,
-                        name: name,
-                        ingredients: ingredients,
-                        preparation: preparation,
-                        experience: experience,
-                        time_needed: time_needed,
-                        number_people: number_people,
-                        created: created,
-                        owner: owner,
-                        rights: rights,
-                    }
-                }).collect() // Collect payments so now `QueryResult` is mapped to `Vec<Payment>`
-            }).unwrap(); // Unwrap `Vec<Payment>`
-    return recipes;
+fn replace_breaklines(text: String) -> String {
+    text.replace("\n", "<br />")
 }
 
 fn main() {
