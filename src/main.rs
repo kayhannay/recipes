af0c59167720f0dc72c867bbf9ef08a87d42f02b
Kay Hannay <klinux@hannay.de>
2020-08-09 23:04:53 +0200

    feat: update to latest rust and rocket

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
+#![feature(proc_macro_hygiene, decl_macro)]
 
-extern crate rocket;
-extern crate rocket_contrib;
-extern crate mysql;
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 extern crate chrono;
 
-use rocket_contrib::Template;
+use rocket_contrib::templates::Template;
+use rocket_contrib::databases::mysql;
 use mysql as my;
+use mysql::params;
 use chrono::NaiveDateTime;
+
+#[database("recipe_db")]
+struct RecipeDatabase(my::Conn);
 
 #[derive(Debug, PartialEq, Eq, Serialize)]
 struct Recipe {
@@ -26,7 +29,7 @@ struct Recipe {
     rights: String,
 }
 
-#[derive(Debug, PartialEq, Eq, Serialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
 struct RecipeName {
     id: i32,
     name: String,
@@ -43,17 +46,15 @@ struct RecipeModel {
 }
 
 #[get("/")]
-fn index() -> Template {
-    let pool = init_db();
-    let _recipe_names = get_recipes_names(pool);
+fn recipe_list(mut connection: RecipeDatabase) -> Template {
+    let _recipe_names = get_recipes_names(&mut connection.0);
     let model = RecipeOverviewModel { recipe_names: _recipe_names };
     Template::render("index", &model)
 }
 
 #[get("/recipe/<id>")]
-fn hello(id: i32) -> Template {
-    let pool = init_db();
-    let _recipe = get_recipe(pool, &id);
+fn recipe(id: i32, mut connection: RecipeDatabase) -> Template {
+    let _recipe = get_recipe(&mut connection.0, &id);
     let model = RecipeModel {
         recipe: _recipe.unwrap(),
     };
@@ -61,57 +62,50 @@ fn hello(id: i32) -> Template {
     Template::render("recipe", &model)
 }
 
-fn init_db() -> my::Pool {
-    let pool = my::Pool::new("mysql://rezepte:my-secret@127.0.0.1:3306/rezepte").unwrap();
-    return pool;
-}
-
-fn get_recipes_names(pool: mysql::Pool) -> Vec<RecipeName> {
-    let recipes: Vec<RecipeName> =
-        pool.prep_exec("SELECT id,name FROM rezepte", ())
-            .map(|result| { // In this closure we will map `QueryResult` to `Vec<Payment>`
-                // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
-                // will map each `MyResult` to contained `row` (no proper error handling)
-                // and second call to `map` will map each `row` to `Payment`
+fn get_recipes_names(connection: &mut my::Conn) -> Vec<RecipeName> {
+    let recipes =
+        connection.prep_exec("SELECT id,name FROM rezepte", ())
+            .map(|result| {
                 result.map(|x| x.unwrap()).map(|row| {
-                    // ⚠️ Note that from_row will panic if you don't follow your schema
                     let (id, name) = my::from_row(row);
                     RecipeName {
-                        id: id,
-                        name: name,
+                        id,
+                        name
                     }
-                }).collect() // Collect payments so now `QueryResult` is mapped to `Vec<Payment>`
-            }).unwrap(); // Unwrap `Vec<Payment>`
+                }).collect()
+            }).unwrap();
     return recipes;
 }
 
-fn get_recipe(pool: mysql::Pool, id: &i32) -> Option<Recipe> {
-    let mut stmt = pool.prepare("SELECT * FROM rezepte WHERE id=?").unwrap();
-    let mut recipe = None;
-    for row in stmt.execute((*id,)).unwrap() {
-        let mut row = row.unwrap();
-        recipe = Some(Recipe {
-            id: row.take("id").unwrap(),
-            category: row.take("category").unwrap(),
-            name: row.take("name").unwrap(),
-            ingredients: replace_breaklines(row.take("ingredients").unwrap()),
-            preparation: replace_breaklines(row.take("preparation").unwrap()),
-            experience: row.take("experience").unwrap(),
-            time_needed: row.take("time_needed").unwrap_or("".to_string()),
-            number_people: row.take("number_people").unwrap(),
-            created: row.take("created").unwrap(),
-            owner: row.take("owner").unwrap(),
-            rights: row.take("rights").unwrap(),
-        })
-    }
+fn get_recipe(connection: &mut my::Conn, id: &i32) -> Option<Recipe> {
+    let mut vector: Vec<Recipe> =
+        connection.prep_exec("SELECT id,category,name,ingredients,preparation,experience,time_need,number_people,created,owner,rights FROM rezepte WHERE id=:id", params! { "id" => id })
+            .map(|result| {
+                result.map(|x| x.unwrap()).map(|row| {
+                    let (id, category, name, ingredients, preparation, experience, time_need, number_people, created, owner, rights) = my::from_row(row);
+                    Recipe {
+                        id,
+                        category,
+                        name,
+                        ingredients,
+                        preparation,
+                        experience,
+                        time_needed: time_need,
+                        number_people,
+                        created,
+                        owner,
+                        rights
+                    }
+                }).collect()
+            }).unwrap();
+    let recipe = vector.pop();
     return recipe;
 }
 
-fn replace_breaklines(text: String) -> String {
-    text.replace("\n", "<br />")
-}
-
 fn main() {
-    rocket::ignite().mount("/", routes![index, hello]).attach(Template::fairing()).launch();
+    rocket::ignite()
+        .mount("/", routes![recipe_list, recipe])
+        .attach(Template::fairing())
+        .attach(RecipeDatabase::fairing())
+        .launch();
 }
-
